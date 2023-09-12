fdad7ab4103f35e9577de48e5446f996e95c8877
Clarionxk <clarion.xk@aol.com>
2023-09-12 12:20:19 +0200

    contact fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div name='contact' className=' w-full h-200vh bg-gradient-to-b from-black to-gray-800 p-4 text-white '>
       
     <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
-        <div className='pt-80 '>
+        <div className='pt-48 '>
             <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
                 Contact
             </p>

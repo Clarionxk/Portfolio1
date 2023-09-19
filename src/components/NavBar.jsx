96015b03071fd7604aee96170d6292cba0471c07
Clarionxk <clarion.xk@aol.com>
2023-09-19 23:12:08 +0200

    Header Upd

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,7 +38,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
         <div>
-            <h1 className='text-5xl font-signature ml-2'>Zeil</h1>
+            <h1 className='text-5xl font-italic ml-2'>Zeilver</h1>
         </div>
 
         <ul className='hidden md:flex'>
